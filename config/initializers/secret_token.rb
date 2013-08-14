0e12e95b784f4e2ae882144af756d79d5dd791d4
D Tucker <danielle@tuckerlabs.com>
2013-08-14 11:59:10 -0700

    Initial setup of stylesheet, removal of public page for emberjs app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '90fe6d4255e7762aeb3aea299fe9cf76d631fa1783290814b0303f70289e73c0b7de38b3a713063740d39af6da4fbb782841bfbd6a9ac15bcbf866cac88313d5'
